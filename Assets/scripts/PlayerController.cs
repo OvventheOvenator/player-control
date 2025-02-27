a62511cc5484b4225d82ac12b5c78cd81ffebc08
Brehm_Owen <breho313@hsd.k12.or.us>
2025-02-26 16:07:45 -0800

    finishing1.3

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -1,7 +1,9 @@
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
-{
+{ //"float" is a speed variable //"private or puplic" is an acces modifier
+    public float speed = 5.0f; //the "f" is for Computer to understand this is float)
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,6 +14,9 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         // move vehicle
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
     }
 }
+
+//deltatime: The amount of seconds took for the engine to process previous frame
+//multiply (*) operator: multiplies its inputs and calculates the result.
