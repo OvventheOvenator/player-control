8b8ef2a8bdd1247d4bb04ab309fb59733e9c0352
Brehm_Owen <breho313@hsd.k12.or.us>
2025-02-28 11:15:10 -0800

    finishing1.4

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -2,8 +2,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 { //"float" is a speed variable //"private or puplic" is an acces modifier
-    public float speed = 5.0f; //the "f" is for Computer to understand this is float)
-
+    private float speed = 20.0f; //the "f" is for Computer to understand this is float)
+    private float turnspeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,10 +15,16 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // move vehicle
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+         //this is where we get player input
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        //moves the car forward based on vertical input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // rotates the car based on horizontal input
+        transform.Rotate(Vector3.up, turnspeed * horizontalInput * Time.deltaTime);
     }
 }
 
 //deltatime: The amount of seconds took for the engine to process previous frame
 //multiply (*) operator: multiplies its inputs and calculates the result.
+//words starting with a lowercase are variables
